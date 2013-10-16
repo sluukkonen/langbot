49de96223fc00ecdc011b92ebd7d8c0989b8db89
Sakumatti Luukkonen <sakumatti.luukkonen@eficode.com>
2013-10-16 20:12:19 +0300

    Use the default ExecutionContext with `blocking` for now.

--- src/main/scala/actions/PythonAction.scala
+++ src/main/scala/actions/PythonAction.scala
@@ -2,31 +2,31 @@ package actions
 
 import response.{Response, Error, Success}
 import scala.concurrent._
-import java.util.concurrent.Executors
 import org.python.util.PythonInterpreter
 import java.io.{PrintStream, ByteArrayOutputStream}
+import scala.concurrent.ExecutionContext.Implicits._
 
 class PythonAction extends Action {
 
-  implicit val executionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(4))
-
   def evaluate(message: String): Future[Response] = {
     future {
-      val python = new PythonInterpreter()
+      blocking {
+        val python = new PythonInterpreter()
 
-      val outputStream = new ByteArrayOutputStream
-      val printStream = new PrintStream(outputStream)
+        val outputStream = new ByteArrayOutputStream
+        val printStream = new PrintStream(outputStream)
 
-      python.setOut(printStream)
-      python.setErr(printStream)
+        python.setOut(printStream)
+        python.setErr(printStream)
 
-      try {
-        val result = python.eval(python.compile(message))
-        Success(result.toString, outputStream.toString)
-      } catch {
-        case e: Exception => Error(e.toString)
-      } finally {
-        python.cleanup()
+        try {
+          val result = python.eval(python.compile(message))
+          Success(result.toString, outputStream.toString)
+        } catch {
+          case e: Exception => Error(e.toString)
+        } finally {
+          python.cleanup()
+        }
       }
     }
   }
--- src/main/scala/actions/RubyAction.scala
+++ src/main/scala/actions/RubyAction.scala
@@ -4,29 +4,30 @@ import org.jruby.{Ruby, RubyInstanceConfig}
 import scala.concurrent._
 import java.io.{PrintStream, ByteArrayOutputStream}
 import response.{Error, Success}
-import java.util.concurrent.Executors
+import scala.concurrent.ExecutionContext.Implicits._
 
 class RubyAction extends Action {
 
-  implicit val executionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(4))
-
   override def evaluate(message: String) = {
     future {
-      val config = new RubyInstanceConfig
+      blocking {
+        val config = new RubyInstanceConfig
 
-      val outputStream = new ByteArrayOutputStream
-      val printStream = new PrintStream(outputStream)
+        val outputStream = new ByteArrayOutputStream
+        val printStream = new PrintStream(outputStream)
 
-      config.setOutput(printStream)
-      config.setError(printStream)
+        config.setOutput(printStream)
+        config.setError(printStream)
 
-      val ruby = Ruby.newInstance(config)
+        val ruby = Ruby.newInstance(config)
 
-      try {
-        val result = ruby.evalScriptlet(message)
-        Success(result.inspect.toString, outputStream.toString)
-      } catch {
-        case e: Exception => Error(e.getMessage)
+        try {
+          val result = ruby.evalScriptlet(message)
+          Success(result.inspect.toString, outputStream.toString)
+        } catch {
+          case e: Exception => Error(e.getMessage)
+        }
+
       }
     }
   }
