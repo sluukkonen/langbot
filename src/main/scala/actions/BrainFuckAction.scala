2769995ca7b04e77bdcc2305ddaa532ad2a1d20c
Sakumatti Luukkonen <sakumatti.luukkonen@eficode.com>
2014-05-19 23:03:50 +0300

    Optimize Brainfuck interpreter

--- src/main/scala/actions/BrainFuckAction.scala
+++ src/main/scala/actions/BrainFuckAction.scala
@@ -3,57 +3,64 @@ package actions
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
 import response.{ErrorResponse, SuccessResponse, Response}
+import scala.collection.mutable
+import scala.annotation.tailrec
 
 class BrainFuckAction extends Action {
 
   def evaluate(program: String): Future[Response] = Future {
 
     val tape = new Array[Int](30000)
-    var ip = 0
-    var dp = 0
+    var ip, dp = 0
+    val jumpTable = new mutable.HashMap[Int, Int]()
     val output = new StringBuilder
 
     try {
       while (ip < program.length) {
         program.charAt(ip) match {
-          case '>'                  => dp += 1
-          case '<'                  => dp -= 1
-          case '+'                  => tape(dp) += 1
-          case '-'                  => tape(dp) -= 1
-          case '.'                  => output.append(tape(dp).asInstanceOf[Char])
-          case '[' if tape(dp) == 0 => ip = jumpForward(program, ip + 1, 1)
-          case ']' if tape(dp) != 0 => ip = jumpBackward(program, ip - 1, 1)
-          case _                    =>
+          case '>' => dp += 1
+          case '<' => dp -= 1
+          case '+' => tape(dp) = (tape(dp) + 1) & 0xff
+          case '-' => tape(dp) = (tape(dp) - 1) & 0xff
+          case '.' => output.append(tape(dp).toChar)
+          case '[' => if (tape(dp) == 0) ip = jumpTable.getOrElseUpdate(ip, jumpForward(program, ip))
+          case ']' => if (tape(dp) != 0) ip = jumpTable.getOrElseUpdate(ip, jumpBackward(program, ip))
+          case _ =>
         }
         ip += 1
       }
+      SuccessResponse(output.mkString)
     } catch {
       case e: Exception => ErrorResponse(e.getMessage)
     }
-
-    SuccessResponse(output.mkString)
   }
 
-  def jumpForward(program: String, ip: Int, depth: Int): Int = {
+  private def jumpForward(program: String, ip: Int): Int = jumpForward(program, ip + 1, 1)
+
+  private def jumpBackward(program: String, ip: Int): Int = jumpBackward(program, ip - 1, 1)
+
+  @tailrec
+  private def jumpForward(program: String, ip: Int, depth: Int): Int = {
     if (depth == 0)
       ip - 1
     else {
       program.charAt(ip) match {
         case '[' => jumpForward(program, ip + 1, depth + 1)
         case ']' => jumpForward(program, ip + 1, depth - 1)
-        case _   => jumpForward(program, ip + 1, depth)
+        case _ => jumpForward(program, ip + 1, depth)
       }
     }
   }
 
-  def jumpBackward(program: String, ip: Int, depth: Int): Int = {
+  @tailrec
+  private def jumpBackward(program: String, ip: Int, depth: Int): Int = {
     if (depth == 0)
       ip + 1
     else {
       program.charAt(ip) match {
         case '[' => jumpBackward(program, ip - 1, depth - 1)
         case ']' => jumpBackward(program, ip - 1, depth + 1)
-        case _   => jumpBackward(program, ip - 1, depth)
+        case _ => jumpBackward(program, ip - 1, depth)
       }
     }
   }
--- src/test/scala/actions/BrainFuckActionSpec.scala
+++ src/test/scala/actions/BrainFuckActionSpec.scala
@@ -1,15 +1,21 @@
 package actions
 
 import org.scalatest.{Matchers, FlatSpec}
-import response.SuccessResponse
+import response.{ErrorResponse, SuccessResponse}
+import scala.io.Source
 
 class BrainFuckActionSpec extends FlatSpec with Matchers with Evaluatable {
 
   override val action = new BrainFuckAction
 
   "A BrainFuckAction" should "evaluate Brainfuck" in {
-    val result = evaluate("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++" +
-      ".------.--------.>+.>.")
-    result should be(SuccessResponse("Hello World!\n"))
+    evaluate(loadProgram("hello")) should be(SuccessResponse("Hello World!\n"))
   }
+
+  it should "be reasonably fast" in {
+    evaluate(loadProgram("benchmark")) should be(SuccessResponse("OK"))
+  }
+
+  private def loadProgram(name: String): String =
+    Source.fromURL(getClass.getResource(s"/brainfuck/${name}.bf")).getLines.mkString
 }
